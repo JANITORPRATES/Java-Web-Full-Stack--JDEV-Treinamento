6bad58e682cf3af4bc1df1933c801edbadcc4934
Janitor Prates <janitorprates@gmail.com>
2022-10-09 10:48:06 -0300

    9.24 - Descobrindo se o aluno está em recuperação

--- primeiro-programa-java/src/cursojava/classes/Aluno.java
+++ primeiro-programa-java/src/cursojava/classes/Aluno.java
@@ -144,12 +144,16 @@ public class Aluno {
 	
 	public String getAlunoAprovado2() {
 		double media = this.getMediaNota();
-		if(media >= 70) {
-			return "Aluno está aprovado";
+		if(media >= 50) {
+			if(media >= 70) {
+				return "Aluno está aprovado";
+			}else {
+				return "Aluno em recuperação";
+			}
 		}else {
 			return "Aluno está reprovado";
 		}
-	}
+	}//Fim getAlunoAprovado2()
 
 	
 	@Override
