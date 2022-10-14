c07d6ad1254ad2829da08a51530a87933a89b65c
Janitor Prates <janitorprates@gmail.com>
2022-10-14 00:26:25 -0300

    10.8 - Entendendo Polimorfismo

--- primeiro-programa-java/src/cursojava/executavel/TestandoClassesFilhas.java
+++ primeiro-programa-java/src/cursojava/executavel/TestandoClassesFilhas.java
@@ -2,6 +2,7 @@ package cursojava.executavel;
 
 import cursojava.classes.Aluno;
 import cursojava.classes.Diretor;
+import cursojava.classes.Pessoa;
 import cursojava.classes.Secretario;
 
 public class TestandoClassesFilhas {
@@ -9,17 +10,17 @@ public class TestandoClassesFilhas {
 	public static void main(String[] argumentos) {
 		
 		Aluno aluno = new Aluno();
-		aluno.setNome("Janio");
+		aluno.setNome("Janio - Aluno");
 		aluno.setNomeEscola("Jdev - treinamentos");
 		aluno.setIdade(16);
 		
 		Diretor diretor = new Diretor();
-		diretor.setNome("Geraldo");
+		diretor.setNome("Geraldo - Diretor");
 		diretor.setRegistroGeral("12365544");
 		diretor.setIdade(50);
 		
 		Secretario secretario = new Secretario();
-		secretario.setNome("Egidio");
+		secretario.setNome("Egidio - Secretario");
 		secretario.setExperiencia("Administração");
 		secretario.setNumeroCpf("1236655");
 		secretario.setIdade(18);
@@ -35,5 +36,14 @@ public class TestandoClassesFilhas {
 		System.out.println("Salario Aluno é : " + aluno.salario());
 		System.out.println("Salario Diretor é : " + diretor.salario());
 		System.out.println("Salario Secretario é : " + secretario.salario());
+		
+		teste(aluno);
+		teste(diretor);
+		teste(secretario);
+	}
+	
+	public static void teste(Pessoa pessoa) {
+		System.out.println("Essa pessoa é demais = " + pessoa.getNome()
+		+ " e o salário é de = " + pessoa.salario());
 	}
 }
