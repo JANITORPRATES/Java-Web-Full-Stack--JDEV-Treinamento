60abefe99fd1f6db8be0d5cfbf24d7049356e89f
Janitor Prates <janitorprates@gmail.com>
2022-10-15 11:20:26 -0300

    13.5 - Diferentes formas de atribuição de valores

--- primeiro-programa-java/src/cursojava/executavel/ArrayVetor.java
+++ primeiro-programa-java/src/cursojava/executavel/ArrayVetor.java
@@ -1,27 +1,37 @@
 package cursojava.executavel;
 
-import javax.swing.JOptionPane;
-
 public class ArrayVetor {
 	
 	public static void main(String[] argumentos) {
 		
-		/* Array pode ser de todos os tipos de dados e objetos também
-		 * Array sempre deve ter a quantidade de posições definidas */
-		
-		String posicoes = JOptionPane.showInputDialog("Quantas posições o Array deve ter?");
-		
-		double[] notas = new double[Integer.parseInt(posicoes)];
+		//Exite vários tipos de Array
+		int[] inteiros = new int[10];
+		float[] floats = new float[10];
 		
 		
-		for(int pos = 0; pos < notas.length; pos++) {
-			String valor = JOptionPane.showInputDialog("Qual o valor da posição = " + pos);
-			notas[pos] = Double.valueOf(valor);
+		
+		/*maneiras de atribuir valores ao Array*/
+		
+						  // 0    1    2    3
+		double[] valores = {9.5, 8.5, 6.8, 100};
+		
+		//Para passar vários tipos de dados para um Array a gente usa um arry de string
+		
+		String[] valores1 = new String[4];
+		
+		valores1[0] = "Janitor";
+		valores1[1] = "90";
+		valores1[2] = "Curso Java";
+		valores1[3] = "janitorcacule@hotmail.com";
+		
+		//Outra forma é passar diretamente
+		String[] valores2 = {"Janitor", "90", "Curso Java", "janitorcacule@hotmail.com"};
+		
+		
+		for(int pos = 0; pos < valores2.length; pos++) {
+			System.out.println("Valor na posição "+ pos + " é igual = "  + valores2[pos]);
 		}
 		
-		for(int pos = 0; pos < notas.length; pos++) {
-			System.out.println("Nota " + (pos +  1) + " é = " + notas[pos]);
-		}
 	}
 
 }
