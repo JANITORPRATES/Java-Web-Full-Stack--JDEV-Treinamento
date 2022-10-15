e897d834d95890b7aa2eef0339f7fda5e4640073
Janitor Prates <janitorprates@gmail.com>
2022-10-15 16:57:46 -0300

    Thread processando em paralelo com o fluxo normal do programa

--- primeiro-programa-java/src/cursojava/thread/ThreadTeste.java
+++ primeiro-programa-java/src/cursojava/thread/ThreadTeste.java
@@ -1,18 +1,40 @@
 package cursojava.thread;
 
+import javax.swing.JOptionPane;
+
 public class ThreadTeste {
 	
 	public static void main(String[] parametros) throws InterruptedException {
 		
-		for(int pos = 0; pos < 10; pos++) {
+		/*Thread processando em paralelo*/
+		new Thread() {
 			
-			/*Quero executar esse envio com um tempo de parada ou com um tempo determinado */
-			System.out.println("Executando alguma rotina, por exemplo envio automático de e-mail");
+			public void run() {//Executa o que nós queremos
+				//Código da rotina
+				
+				/*Código da rotina que eu quero executar em paralelo*/
+				for(int pos = 0; pos < 10; pos++) {
+					
+					/*Quero executar esse envio com um tempo de parada ou com um tempo determinado */
+					System.out.println("Executando alguma rotina, por exemplo envio automático de e-mail");
+					
+					try {
+						Thread.sleep(1000);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}// Dá um tempo
+				}
+				//Fim do código em paralelo
+				
+			}
 			
-			Thread.sleep(1000);// Dá um tempo
-		}
+		}.start();/*Start liga a thread que fica processando paralelamente por trás*/
 		
+		
+		/*Código do sistema do usuário continua o fluxo de trabalho*/
 		System.out.println("CHEGOU AO FIM DO CÓDIGO DE TESTE DE THREAD");
+		/*Fluxo do sistema, cadastro de venda, uma emissão d enota fiscal, algo do tipo*/
+		JOptionPane.showMessageDialog(null, "Sistema continua executando para o usuário");
 	}
 
 }
