069321c9f86d5e5607607950c2daf2db2a906b4d
Janitor Prates <janitorprates@gmail.com>
2022-10-15 15:50:16 -0300

    13.13 - Lendo uma Matriz multidimensional

--- primeiro-programa-java/src/cursojava/executavel/Matriz.java
+++ primeiro-programa-java/src/cursojava/executavel/Matriz.java
@@ -14,5 +14,14 @@ public class Matriz {
 		notas[1][1] = 70;
 		notas[1][2] = 30;
 		
+		/*Percorre as linhas*/
+		for (int posLinha = 0; posLinha < notas.length; posLinha++) {
+			System.out.println("********************************************************");
+			/*Para cada linha percorre as colunas do ARRAY*/
+			for (int posColuna = 0; posColuna < notas[posLinha].length; posColuna++) {
+				System.out.println("Valor da matriz " + (posLinha + 1) 
+						+ ": " + notas[posLinha][posColuna]);
+			}
+		}
 	}
 }
