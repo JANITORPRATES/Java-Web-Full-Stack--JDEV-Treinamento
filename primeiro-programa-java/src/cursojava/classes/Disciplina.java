ede3a11fec4ae8cc977f1e84a9c3ba35c7c0f6f2
Janitor Prates <janitorprates@gmail.com>
2022-10-15 13:06:50 -0300

    13.6 - Adicionando o array de notas dentro da disciplina

--- primeiro-programa-java/src/cursojava/classes/Disciplina.java
+++ primeiro-programa-java/src/cursojava/classes/Disciplina.java
@@ -1,10 +1,13 @@
 package cursojava.classes;
 
+import java.util.Arrays;
+
 /*Essa classe servirá para todos os objetos e instancias de notas e materias/disciplinas*/
 public class Disciplina {
 
 	private String disciplina;
-	private double nota;
+	//Cada disciplina terá 4 notas durante o ano inteiro
+	private double[] nota = new double[4];
 
 	public String getDisciplina() {
 		return disciplina;
@@ -14,22 +17,32 @@ public class Disciplina {
 		this.disciplina = disciplina;
 	}
 
-	public double getNota() {
+
+	public double[] getNota() {
 		return nota;
 	}
 
-	public void setNota(double nota) {
+	public void setNota(double[] nota) {
 		this.nota = nota;
 	}
+	
+	public double getMediaNotas() {
+		double somaTotal = 0;
+		
+		for (int pos = 0; pos < nota.length; pos++) {
+			somaTotal += nota[pos];
+		}
+		
+		return somaTotal / 4;
+	}
+	
 
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + ((disciplina == null) ? 0 : disciplina.hashCode());
-		long temp;
-		temp = Double.doubleToLongBits(nota);
-		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + Arrays.hashCode(nota);
 		return result;
 	}
 
@@ -47,14 +60,17 @@ public class Disciplina {
 				return false;
 		} else if (!disciplina.equals(other.disciplina))
 			return false;
-		if (Double.doubleToLongBits(nota) != Double.doubleToLongBits(other.nota))
+		if (!Arrays.equals(nota, other.nota))
 			return false;
 		return true;
 	}
 
 	@Override
 	public String toString() {
-		return "Disciplina [disciplina=" + disciplina + ", nota=" + nota + "]";
+		return "Disciplina [disciplina=" + disciplina + ", nota=" + Arrays.toString(nota) + "]";
 	}
+
+	
+	
 
 }// Fim da classe
--- primeiro-programa-java/src/cursojava/executavel/ArrayVetor.java
+++ primeiro-programa-java/src/cursojava/executavel/ArrayVetor.java
@@ -1,36 +1,31 @@
 package cursojava.executavel;
 
+import cursojava.classes.Aluno;
+import cursojava.classes.Disciplina;
+
 public class ArrayVetor {
 	
 	public static void main(String[] argumentos) {
 		
-		//Exite vários tipos de Array
-		int[] inteiros = new int[10];
-		float[] floats = new float[10];
+		double[] notas = {8.8,9.7,7.6,6.8};
+		double[] notasLogica = {7.8,8.7,5.6,9.8};
 		
+		/*Criação do aluno*/
+		Aluno aluno = new Aluno();
+		aluno.setNome("Jânitor");
+		aluno.setNomeEscola("Jdev Treinamentos");
 		
+		//Criação da disciplina
+		Disciplina disciplina = new Disciplina();
+		disciplina.setDisciplina("Curso de Java");
+		disciplina.setNota(notas);
 		
-		/*maneiras de atribuir valores ao Array*/
+		Disciplina disciplina2 = new Disciplina();
+		disciplina.setDisciplina("Lógica de programação");
+		disciplina.setNota(notasLogica);
 		
-						  // 0    1    2    3
-		double[] valores = {9.5, 8.5, 6.8, 100};
-		
-		//Para passar vários tipos de dados para um Array a gente usa um arry de string
-		
-		String[] valores1 = new String[4];
-		
-		valores1[0] = "Janitor";
-		valores1[1] = "90";
-		valores1[2] = "Curso Java";
-		valores1[3] = "janitorcacule@hotmail.com";
-		
-		//Outra forma é passar diretamente
-		String[] valores2 = {"Janitor", "90", "Curso Java", "janitorcacule@hotmail.com"};
-		
-		
-		for(int pos = 0; pos < valores2.length; pos++) {
-			System.out.println("Valor na posição "+ pos + " é igual = "  + valores2[pos]);
-		}
+		aluno.getDisciplinas().add(disciplina);
+		aluno.getDisciplinas().add(disciplina2);
 		
 	}
 
