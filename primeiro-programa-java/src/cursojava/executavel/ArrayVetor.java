69ec4d757d02663384915b090422af2765456d12
Janitor Prates <janitorprates@gmail.com>
2022-10-15 11:05:21 -0300

    13.4 - Entrada e leitura de forma dinâmica

--- primeiro-programa-java/src/cursojava/executavel/ArrayVetor.java
+++ primeiro-programa-java/src/cursojava/executavel/ArrayVetor.java
@@ -1,18 +1,23 @@
 package cursojava.executavel;
 
+import javax.swing.JOptionPane;
+
 public class ArrayVetor {
 	
 	public static void main(String[] argumentos) {
 		
 		/* Array pode ser de todos os tipos de dados e objetos também
 		 * Array sempre deve ter a quantidade de posições definidas */
-		double[] notas = new double[5];
 		
-		/*Atribuir valor nas posições do arrays*/
-		notas[0] = 8.5;
-		notas[1] = 7.6;
-		notas[2] = 6.8;
-		notas[3] = 9.5;
+		String posicoes = JOptionPane.showInputDialog("Quantas posições o Array deve ter?");
+		
+		double[] notas = new double[Integer.parseInt(posicoes)];
+		
+		
+		for(int pos = 0; pos < notas.length; pos++) {
+			String valor = JOptionPane.showInputDialog("Qual o valor da posição = " + pos);
+			notas[pos] = Double.valueOf(valor);
+		}
 		
 		for(int pos = 0; pos < notas.length; pos++) {
 			System.out.println("Nota " + (pos +  1) + " é = " + notas[pos]);
