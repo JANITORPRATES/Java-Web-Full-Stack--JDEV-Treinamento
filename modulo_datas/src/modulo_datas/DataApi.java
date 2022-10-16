fc192663e750739de9abfeabbb1b246555a6bb9e
Janitor Prates <janitorprates@gmail.com>
2022-10-16 20:35:38 -0300

    15.8 - Nova API de Data e Hora do Java e 15.9 - Formatando data e hora

---
+++ modulo_datas/src/modulo_datas/DataApi.java
@@ -0,0 +1,31 @@
+package modulo_datas;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+public class DataApi {
+	
+	public static void main(String[] args) {
+		
+		/*Nova API de data a partir do Java 8*/
+		
+		LocalDate dataAtual = LocalDate.now();
+		
+		System.out.println("Data atual: " + dataAtual.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+		
+		LocalTime horaAtual = LocalTime.now();
+		
+		System.out.println("Hora atual: " + horaAtual.format(DateTimeFormatter.ofPattern("HH:mm")));
+		
+		LocalDateTime dataAtualHoraAtual = LocalDateTime.now();
+		
+		System.out.println("Data e hora atual: " 
+					+ dataAtualHoraAtual.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+		
+	}
+	
+
+}
