246b2af2840e40a9d4d7cfbf073fe8cf0b6b895a
Janitor Prates <janitorprates@gmail.com>
2022-10-16 10:20:41 -0300

    Conhecendo o GridBagConstraints gerenciador de posicionamento e layout

--- primeiro-programa-java/src/cursojava/thread/TelaTimeThread.java
+++ primeiro-programa-java/src/cursojava/thread/TelaTimeThread.java
@@ -1,10 +1,24 @@
 package cursojava.thread;
 
 import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.BorderLayout;
+import java.awt.GridBagLayout;
 
 import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class TelaTimeThread extends JDialog {
+	
+	private JPanel jPanel = new JPanel(new GridBagLayout()); //Painel de componentes
+	
+	private JLabel descricaoHora = new JLabel("Time Thread 1");
+	private JTextField mostraTempo = new JTextField();
+	
+	private JLabel descricaoHora2 = new JLabel("Time Thread 2");
+	private JTextField mostraTempo2 = new JTextField();
 	
 	public TelaTimeThread() { /*Executa o que tiver dentro no momento da abertura ou execução*/
 		setTitle("Minha tela de time com Thread");
@@ -13,7 +27,26 @@ public class TelaTimeThread extends JDialog {
 		setResizable(false);//permite ou não redimensionar a tela
 		/*Primeira parte concluída*/
 		
+		GridBagConstraints gridBagConstraints = new GridBagConstraints();//Controlar o posicionamento
+		gridBagConstraints.gridx = 0;
+		gridBagConstraints.gridy = 0;
 		
+		descricaoHora.setPreferredSize(new Dimension(200, 25));
+		jPanel.add(descricaoHora, gridBagConstraints);
+
+		mostraTempo.setPreferredSize(new Dimension(200, 25));
+		gridBagConstraints.gridy++;
+		jPanel.add(mostraTempo, gridBagConstraints);
+		
+		descricaoHora2.setPreferredSize(new Dimension(200, 25));
+		gridBagConstraints.gridy++;
+		jPanel.add(descricaoHora2, gridBagConstraints);
+		
+		mostraTempo2.setPreferredSize(new Dimension(200, 25));
+		gridBagConstraints.gridy++;
+		jPanel.add(mostraTempo2, gridBagConstraints);
+		
+		add(jPanel, BorderLayout.WEST);
 		
 		
 		//Sempre será o último comando
