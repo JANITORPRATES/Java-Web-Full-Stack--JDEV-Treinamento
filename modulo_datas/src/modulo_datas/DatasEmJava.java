3931a8cd0c24778057ea23ce3d8bcf3548601fbf
Janitor Prates <janitorprates@gmail.com>
2022-10-16 14:53:59 -0300

    15.4 - After comparando datas maiores - data 1 é maior que a data 2

--- modulo_datas/src/modulo_datas/DatasEmJava.java
+++ modulo_datas/src/modulo_datas/DatasEmJava.java
@@ -9,50 +9,18 @@ public class DatasEmJava {
 
 	public static void main(String[] args) throws ParseException {
 
-		Calendar calendario = Calendar.getInstance();
+		SimpleDateFormat formatoData = new SimpleDateFormat("dd/MM/yyyy");
 		
-		Date data = new Date();
+		Date dataVencimentoBoleto = formatoData.parse("18/10/2022");
 		
-		System.out.println("Data em milisegundos: " + data.getTime());
-		System.out.println("Calendar em milisegundos: " + calendario.getTimeInMillis());
+		Date dataAtualHoje = formatoData.parse("16/10/2022");
 		
-		System.out.println("Dia do mês: " + data.getDate());
-		System.out.println("Calendar dia do mês: " + calendario.get(Calendar.DAY_OF_MONTH));
-		
-		System.out.println("Dia da semana: " + data.getDay());
-		System.out.println("Calendar dia da semana: " + calendario.get(Calendar.DAY_OF_WEEK));
-		
-		System.out.println("Hora do dia: " + data.getHours());
-		System.out.println("Calendar hora do dia: " + calendario.get(Calendar.HOUR_OF_DAY));
-		
-		System.out.println("Minuto da hora: " + data.getMinutes());
-		System.out.println("Calendar minuto da hora: " + calendario.get(Calendar.MINUTE));
-		
-		System.out.println("Segundos: " + data.getSeconds());
-		System.out.println("Calendar segundos: " + calendario.get(Calendar.SECOND));
-		
-		System.out.println("Ano: " + (data.getYear() + 1900));
-		System.out.println("Calendar ano: " + calendario.get(Calendar.YEAR));
-		
-		/*---------------------- Simple Date Format -----------------------*/
-		
-		SimpleDateFormat formatoSimplesData = new SimpleDateFormat("dd/MM/yyyy HH:mm.ss");
-		
-		System.out.println("Data atual em formato padrao e String: " + formatoSimplesData.format(data));
-		System.out.println("Calendar data atual em formato padrao e String: " + 
-		formatoSimplesData.format(calendario.getTime()));
-		
-		formatoSimplesData = new SimpleDateFormat("yyyy-MM-dd HH:mm.ss");
-		
-		System.out.println("Data em formato para banco de dados: " + formatoSimplesData.format(data));
-		System.out.println("Calendar data em formato para banco de dados: " + formatoSimplesData.format(calendario.getTime()));
-		
-		System.out.println("Objeto Date: " + formatoSimplesData.parse("1985-09-22 07:20.10"));
-		
-		formatoSimplesData = new SimpleDateFormat("dd/MM/yyyy");
-		
-		System.out.println("Objeto Date (Palloma): " + formatoSimplesData.parse("28/02/1993"));
-		
+		/*Se a data 1 é maior que a data 2*/
+		if(dataVencimentoBoleto.after(dataAtualHoje)) {//Posterior ou maior ou depois da data atual
+			System.out.println("O boleto não está vencido");
+		}else {
+			System.out.println("O boleto está vencido - URGENTE");
+		}
 	}
 
 }
