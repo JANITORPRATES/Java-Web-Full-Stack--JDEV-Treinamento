3a196bfdba6adc7f7f9474cd7290caa341de51fe
Janitor Prates <janitorprates@gmail.com>
2022-10-16 11:00:18 -0300

    14.8 - Adicionando os botões JButton

--- primeiro-programa-java/src/cursojava/thread/TelaTimeThread.java
+++ primeiro-programa-java/src/cursojava/thread/TelaTimeThread.java
@@ -4,7 +4,9 @@ import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.BorderLayout;
 import java.awt.GridBagLayout;
+import java.awt.Insets;
 
+import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -20,9 +22,12 @@ public class TelaTimeThread extends JDialog {
 	private JLabel descricaoHora2 = new JLabel("Time Thread 2");
 	private JTextField mostraTempo2 = new JTextField();
 	
+	private JButton jButton = new JButton("Start");
+	private JButton jButton2 = new JButton("Stop");
+	
 	public TelaTimeThread() { /*Executa o que tiver dentro no momento da abertura ou execução*/
 		setTitle("Minha tela de time com Thread");
-		setSize(new Dimension(300, 300));
+		setSize(new Dimension(240, 240));
 		setLocationRelativeTo(null);//centraliza na tela
 		setResizable(false);//permite ou não redimensionar a tela
 		/*Primeira parte concluída*/
@@ -30,12 +35,16 @@ public class TelaTimeThread extends JDialog {
 		GridBagConstraints gridBagConstraints = new GridBagConstraints();//Controlar o posicionamento
 		gridBagConstraints.gridx = 0;
 		gridBagConstraints.gridy = 0;
+		gridBagConstraints.gridwidth = 2;
+		gridBagConstraints.insets = new Insets(5, 10, 5, 5);
+		gridBagConstraints.anchor = GridBagConstraints.WEST;
 		
 		descricaoHora.setPreferredSize(new Dimension(200, 25));
 		jPanel.add(descricaoHora, gridBagConstraints);
 
 		mostraTempo.setPreferredSize(new Dimension(200, 25));
 		gridBagConstraints.gridy++;
+		mostraTempo.setEditable(false);
 		jPanel.add(mostraTempo, gridBagConstraints);
 		
 		descricaoHora2.setPreferredSize(new Dimension(200, 25));
@@ -44,11 +53,23 @@ public class TelaTimeThread extends JDialog {
 		
 		mostraTempo2.setPreferredSize(new Dimension(200, 25));
 		gridBagConstraints.gridy++;
+		mostraTempo2.setEditable(false);
 		jPanel.add(mostraTempo2, gridBagConstraints);
 		
+		/**************************************************/
+		
+		gridBagConstraints.gridwidth = 1;
+		
+		jButton.setPreferredSize(new Dimension(92, 25));
+		gridBagConstraints.gridy++;
+		jPanel.add(jButton, gridBagConstraints);
+		
+		jButton2.setPreferredSize(new Dimension(92, 25));
+		gridBagConstraints.gridx++;
+		jPanel.add(jButton2, gridBagConstraints);
+		
+		
 		add(jPanel, BorderLayout.WEST);
-		
-		
 		//Sempre será o último comando
 		setVisible(true);/*Torna a tela visível para o usuário*/
 	}
