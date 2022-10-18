832f8974df15e110c6941451a5b22755c61588d2
Janitor Prates <janitorprates@gmail.com>
2022-10-18 00:51:34 -0300

    Escrevendo no arquivo.txt os dados do objeto Pessoa

--- arquivos/src/arquivos/Arquivos.java
+++ arquivos/src/arquivos/Arquivos.java
@@ -3,11 +3,34 @@ package arquivos;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Arquivos {
 
 	public static void main(String[] args) throws IOException {
 		
+		Pessoa pessoa1 = new Pessoa();
+		pessoa1.setNome("Jânitor");
+		pessoa1.setEmail("janitorcacule@hotmail.com");
+		pessoa1.setIdade(37);
+		
+		Pessoa pessoa2 = new Pessoa();
+		pessoa2.setNome("Palloma");
+		pessoa2.setEmail("palloma.cle@hotmail.com");
+		pessoa2.setIdade(29);
+		
+		Pessoa pessoa3 = new Pessoa();
+		pessoa3.setNome("Nalva");
+		pessoa3.setEmail("nalva@hotmail.com");
+		pessoa3.setIdade(58);
+		
+		List<Pessoa> pessoas = new ArrayList();
+		pessoas.add(pessoa1);
+		pessoas.add(pessoa2);
+		pessoas.add(pessoa3);
+		
+		/*Pode vir do banco de dados ou qualquer fonte de dados*/
 		File arquivo = new File("E:\\CODIGOS\\Java-Web-Full-Stack-JDEV-Treinamento\\arquivos\\src\\arquivos\\arquivo.txt");
 		
 		if(!arquivo.exists()) {
@@ -16,8 +39,9 @@ public class Arquivos {
 		
 		FileWriter escreverNoArquivo = new FileWriter(arquivo);
 		
-		for(int i = 1; i <= 10; i++) {
-			escreverNoArquivo.write("Texto da minha linha " + i + "\n");
+		for (Pessoa p : pessoas) {
+			escreverNoArquivo.write(p.getNome() + ";" + p.getEmail() + ";" + p.getIdade() + "\n");
+			
 		}
 		escreverNoArquivo.flush();
 		escreverNoArquivo.close();
