27ccd13adffb03ea6ac3febea0ae2f407203e7e3
Janitor Prates <janitorprates@gmail.com>
2022-10-22 20:07:06 -0300

    21.10 - Criando nosso INSERT ao banco de dados

--- pos-java-jdbc/src/main/java/dao/UserPosDAO.java
+++ pos-java-jdbc/src/main/java/dao/UserPosDAO.java
@@ -1,8 +1,11 @@
 package dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 import conexaojdbc.SingleConnection;
+import model.UserPosJava;
 
 public class UserPosDAO {
 
@@ -10,5 +13,22 @@ public class UserPosDAO {
 	
 	public UserPosDAO() {
 		connection = SingleConnection.getConnection();
+	}
+	
+	public void salvar(UserPosJava objetoUserPosJava) {
+		String sql = "INSERT INTO userposjava (id, nome, email) VALUES (?, ?, ?)";
+		try {
+			PreparedStatement crud_C = connection.prepareStatement(sql);
+			crud_C.setLong(1, 3);
+			crud_C.setString(2, "Palloma Guimarães");
+			crud_C.setString(3, "palloma.cle@hotmail.com");
+			crud_C.execute();
+			connection.commit();// Salva no banco de dados
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 }
--- pos-java-jdbc/src/test/java/pos_java_jdbc/pos_java_jdbc/TesteBancoJdbc.java
+++ pos-java-jdbc/src/test/java/pos_java_jdbc/pos_java_jdbc/TesteBancoJdbc.java
@@ -2,12 +2,18 @@ package pos_java_jdbc.pos_java_jdbc;
 
 import org.junit.Test;
 
-import conexaojdbc.SingleConnection;
+//import conexaojdbc.SingleConnection;
+import dao.UserPosDAO;
+import model.UserPosJava;
 
 public class TesteBancoJdbc {
 
 	@Test
 	public void initBanco() {
-		SingleConnection.getConnection();
+		//SingleConnection.getConnection();
+		UserPosDAO userPosDAO = new UserPosDAO();
+		UserPosJava userposjava = new UserPosJava();
+		
+		userPosDAO.salvar(userposjava);
 	}
 }
